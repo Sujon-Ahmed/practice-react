5854212e8dfddaeccba034c889861b7df960c55f
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-08-13 21:57:18 +0600

    button clicked event with class component

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 
 class ClassComponent extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Designation: {this.props.designation}</h3>
-            </div>
-        );
-    }
+  showMsg() {
+    alert('Class Button Clicked! 🤩');
+  }
+  render() {
+    return (
+      <div>
+        <br></br>
+        <button onClick={this.showMsg}>Click Me! 🙂</button>
+        {/* <h3>Designation: {this.props.designation}</h3> */}
+      </div>
+    );
+  }
 }
 
 export default ClassComponent;
