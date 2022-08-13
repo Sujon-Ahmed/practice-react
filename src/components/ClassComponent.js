638b8fa2b213c51d7ed69bda61f11e9f0287174d
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-08-13 22:07:19 +0600

    pass argument click event by class component

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 class ClassComponent extends Component {
-  showMsg() {
-    alert('Class Button Clicked! 🤩');
+  showMsg(msg) {
+    alert(msg);
   }
   render() {
     return (
       <div>
         <br></br>
-        <button onClick={this.showMsg}>Click Me! 🙂</button>
+        <button onClick={this.showMsg.bind(this, 'This Message will return Class Component!')}>Click Me! 🙂</button>
         {/* <h3>Designation: {this.props.designation}</h3> */}
       </div>
     );
