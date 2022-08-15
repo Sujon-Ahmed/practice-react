22b4abf72a448c3407c2925c5fce7224e5220925
Sujon-Ahmed <sujonahmed5284@gmail.com>
2022-08-15 11:47:55 +0600

    Nested State Example

--- src/components/StateExample.js
+++ src/components/StateExample.js
@@ -5,14 +5,20 @@ class StateExample extends React.Component {
         super();
         this.state = {
             name: 'Sujon Ahmed Riman',
-            age: '22',
-            designation: 'Web Application Developer'
+            age: ['20', '21', '22', '23', '24', '25'],
+            designation: 'Web Application Developer',
+            studyYear: {
+                diploma: '2017-2022',
+                ssc: '2017',
+                jsc: '2015'
+            }
         }
     }
     render() {
         return(
             <div>
-                <h2>I am {this.state.name} a {this.state.designation} but i am only {this.state.age}</h2>
+                <h2>I am {this.state.name} a {this.state.designation} but i am only {this.state.age[2]}</h2>
+                <p>{this.state.studyYear.diploma}</p>
             </div>
         );
     }
